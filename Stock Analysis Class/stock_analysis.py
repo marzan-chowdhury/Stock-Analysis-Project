a815982d032b1e58fc682c05d0a2dc5357edf5a8
marzan-chowdhury <marzan_chowdhury@outlook.com>
2021-03-06 22:15:21 +0000

    separated DataFrame and stock data in method

--- Stock Analysis Class/stock_analysis.py
+++ Stock Analysis Class/stock_analysis.py
@@ -7,27 +7,30 @@ class Stock:
     def __init__(self, stock_ticker):
         self.stock_ticker =  stock_ticker
 
-    # def get_ticker(self): 
-    #     user_ticker_symbol = self.stock_ticker
-    #     ticker_symbol = yf.Ticker(user_ticker_symbol)
-    #     return ticker_symbol
-
     def ticker_data(self, start, end): 
         
-        #start_date = dt.datetime(start)
-        #end_date = dt.datetime(end)
+        # Return the stock information with the provided dates given 
 
         start_date = start
         end_date = end
 
         user_ticker_symbol = self.stock_ticker
-        #ticker_symbol = yf.Ticker(user_ticker_symbol)
 
         df = data.DataReader(user_ticker_symbol, 'yahoo', start_date, end_date)
-        return df.head(15)
+        #return df.head(15)
+        return df
+
+    def present_stock_data(self, start, end): 
+
+        # Present the stock data in a DataFrame 
+
+        stock_data = self.ticker_data(start, end)
+        return stock_data.head(15) 
 
     def start_date(self, start_year, start_month, start_day): 
         
+        #Return the date in a format that is accepted by python 
+
         start_year = start_year
         start_month = start_month 
         start_day = start_day
@@ -38,6 +41,8 @@ class Stock:
     
     def end_date(self, end_year, end_month, end_day): 
 
+        #Return the date in a format that is accepted by python 
+
         end_year = end_year
         end_month = end_month 
         end_day = end_day
@@ -47,12 +52,13 @@ class Stock:
         return end_date 
 
 
-
-microsoft = Stock('msft')
+symbols = ["MSFT","INTC", "AMZN", "EBAY", "AAPL", "GOOG", "FB", "TSLA", "GE"]
+stock_list = Stock(symbols)
 #start = dt.datetime(2021,2,18)
 #end = dt.datetime(2021,3,19) 
-start = microsoft.start_date(2021,2,18)
-end = microsoft.end_date(2021,3,5)
-print(microsoft.ticker_data(start, end))
+start = stock_list.start_date(2021,2,18)
+end = stock_list.end_date(2021,3,5)
+#print(stock_list.ticker_data(start, end))
+print(stock_list.present_stock_data(start, end))
 
 
